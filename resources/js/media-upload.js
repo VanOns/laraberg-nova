dbd845cd86be92ce68786e0618bbb239cfc95eb5
Maurice Wijnia <maurice@van-ons.nl>
2022-01-25 13:32:50 +0100

    [Media] Fix issue with uploading multiple files

--- resources/js/media-upload.js
+++ resources/js/media-upload.js
@@ -1,11 +1,9 @@
+import uuidv4 from './uuidv4'
+
 const mediaUpload = (resourceName, attribute, draftId, onError = () => {}) => {
     const endpoint = `/nova-vendor/laraberg-nova/${resourceName}/attachment/${attribute}`
 
     return async ({filesList, onFileChange}) => {
-        // Call onFileChange with an array of object URLs to trigger the loading animation
-        const files = Array.from(filesList).map(f => ({ url: window.URL.createObjectURL(f) }))
-        onFileChange(files)
-
         // Start uploading the files
         const promises = Array.from(filesList).map(async (file) => {
             const data = new FormData()
@@ -15,13 +13,19 @@ const mediaUpload = (resourceName, attribute, draftId, onError = () => {}) => {
 
             try {
                 const res = await Nova.request().post(endpoint, data)
-                return { url: res.data.url }
+
+                return {
+                  id: uuidv4(),
+                  name: file.name,
+                  url: res.data.url
+                }
             } catch (e) {
                 onError(e)
             }
         })
 
-        onFileChange(await Promise.all(promises))
+        const uploadedFiles = await Promise.all(promises)
+        onFileChange(uploadedFiles)
     }
 }
 
